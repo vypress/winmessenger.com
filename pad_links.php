0f09acee51c27937e10f3f1521c9aa174517ac99
vypress <support@vypress.com>
2024-02-01 09:48:33 +0300

    Update pad_links.php

--- pad_links.php
+++ pad_links.php
@@ -9,13 +9,13 @@ define('WM_FILE_SIZE64', '1.29');
 define('WM_FILE_SIZE_RUS', '1.26');
 define('WM_FILE_SIZE64_RUS', '1.33');
 
-define('DOWNLOAD_1_28', '<a type="application/octet-stream" href="http://www.winmessenger.com/files/WinMessenger2805.msi">WinMessenger2805.msi</a>');
-define('DOWNLOAD_2_28', '<a type="application/octet-stream" href="http://www.vypress.com/ftp/WinMessenger2805.msi">WinMessenger2805.msi</a>');
+define('DOWNLOAD_1_28', '<a type="application/octet-stream" href="https://www.winmessenger.com/files/WinMessenger2805.msi">WinMessenger2805.msi</a>');
+define('DOWNLOAD_2_28', '<a type="application/octet-stream" href="https://www.vypress.com/ftp/WinMessenger2805.msi">WinMessenger2805.msi</a>');
 define('DOWNLOAD_3_28', '<a type="application/octet-stream" href=""></a>');
 define('DOWNLOAD_28_RUS', '<a type="application/octet-stream" href="../files/WinMessenger2805Rus.msi">WinMessenger2805Rus.msi</a>');
 
-define('DOWNLOAD_1_28x64', '<a type="application/octet-stream" href="http://www.winmessenger.com/files/WinMessenger2805x64.msi">WinMessenger2805x64.msi</a>');
-define('DOWNLOAD_2_28x64', '<a type="application/octet-stream" href="http://www.vypress.com/ftp/WinMessenger2805x64.msi">WinMessenger2805x64.msi</a>');
+define('DOWNLOAD_1_28x64', '<a type="application/octet-stream" href="https://www.winmessenger.com/files/WinMessenger2805x64.msi">WinMessenger2805x64.msi</a>');
+define('DOWNLOAD_2_28x64', '<a type="application/octet-stream" href="https://www.vypress.com/ftp/WinMessenger2805x64.msi">WinMessenger2805x64.msi</a>');
 define('DOWNLOAD_3_28x64', '<a type="application/octet-stream" href="x64">x64</a>');
 define('DOWNLOAD_28x64_RUS', '<a type="application/octet-stream" href="../files/WinMessenger2805x64Rus.msi">WinMessenger2805x64Rus.msi</a>');
 ?>
